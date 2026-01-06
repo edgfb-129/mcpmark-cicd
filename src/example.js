3a84e5693f7ce8c66cdace6b55a7fba9306dfbdd
edgfb-129 <fy2rd4li@047756.xyz>
2026-01-07 00:24:39 +0800

    Add ESLint config, workflow, and example with linting errors

---
+++ src/example.js
@@ -0,0 +1,10 @@
+// This file has intentional linting errors
+const unusedVariable = "I'm not used";
+
+console.log("Hello world");
+
+function myFunction() {
+  return "no semicolon"
+}
+
+const stringWithDoubleQuotes = "This uses double quotes";
